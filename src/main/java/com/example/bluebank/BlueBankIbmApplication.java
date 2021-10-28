96a0cac5e1f41be3f528f9a726cb64596ab95bae
Luan Henrique <luanhenrique2@hotmail.com>
2021-10-28 09:33:48 -0300

    Initial commit

---
+++ src/main/java/com/example/bluebank/BlueBankIbmApplication.java
@@ -0,0 +1,13 @@
+package com.example.bluebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankIbmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bluebank/BlueBankIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bluebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
